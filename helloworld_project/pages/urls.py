9330b2dfed5a1a381663461d4d09d0e452646f91
Tazana2 <danisanmez@gmail.com>
2025-02-18 18:36:28 -0500

    Tutorial Sesion

--- helloworld_project/pages/urls.py
+++ helloworld_project/pages/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView, ContactPageView
+from . import views
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
-    path("about/", AboutPageView.as_view(), name="about"),
-    path('products/', ProductIndexView.as_view(), name='index'),
-    path('products/create', ProductCreateView.as_view(), name='form'),
-    path('products/<str:id>', ProductShowView.as_view(), name='show'),
-    path("contact/",ContactPageView.as_view(),name='contact'),
+    path("", views.HomePageView.as_view(), name="home"),
+    path("about/", views.AboutPageView.as_view(), name="about"),
+    path('products/', views.ProductIndexView.as_view(), name='index'),
+    path('products/create', views.ProductCreateView.as_view(), name='form'),
+    path('products/<str:id>', views.ProductShowView.as_view(), name='show'),
+    path('contact/', views.ContactPageView.as_view(),name='contact'),
+    path('cart', views.CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', views.CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', views.CartRemoveAllView.as_view(), name='cart_removeAll'),
 ]
